c2b6514fbdf61dbb9c81f8e9619a8e6f79c5d28c
GenJune <julia.nepochatova@gmail.com>
2026-03-22 21:29:33 +0300

    List template minimum

--- nepochatova.yulia/S1/List.h
+++ nepochatova.yulia/S1/List.h
@@ -5,12 +5,11 @@
 #include <utility>
 
 namespace nepochatova {
-  template<class T>
-  class List;
-
+  template<class T> class List;
   template<class T>
   class LIter {
     friend class List<T>;
+
   };
 
   template<class T>
@@ -20,6 +19,19 @@ namespace nepochatova {
 
   template<class T>
   class List {
+    friend class LIter< T >;
+    friend class LCIter< T >;
+
+  private:
+    struct Node {
+      T data;
+      Node* next;
+      Node* prev;
+
+      Node(const T& value) : data(value), prev(nullptr), next(nullptr) {}
+    };
+    Node* head;
+    size_t list_size;
   };
-}
+};
 #endif
