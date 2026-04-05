cdbdb29b78246c68a219da14cc99a2ac3fd7d8ec
GenJune <julia.nepochatova@gmail.com>
2026-04-05 22:09:37 +0300

    Implemented stack and queue

--- nepochatova.yulia/S2/queue.h
+++ nepochatova.yulia/S2/queue.h
@@ -1,13 +1,13 @@
 #ifndef QUEUE_HPP
 #define QUEUE_HPP
 
-#include "../common/BiList.hpp"
+#include "../common/List.hpp"
+#include <stdexcept>
+#include <utility>
 
-namespace nepochatova
-{
+namespace nepochatova {
   template < class T >
-  class Queue
-  {
+  class Queue {
   private:
     List< T > data_;
 
@@ -16,10 +16,55 @@ namespace nepochatova
     T drop();
     T & front();
     const T & front() const;
-    bool empty() const;
-    size_t size() const;
+    bool empty() const noexcept;
+    size_t size() const noexcept;
     void clear();
   };
-}
 
+  template < class T >
+   void Queue<T>::push(const T& value) {
+    data_.push_back(value);
+  }
+
+  template < class T >
+  T Queue<T>::drop() {
+    if (empty()) {
+      throw std::out_of_range("Queue is empty");
+    }
+    T value = std::move(data_.front());
+    data_.pop_front();
+    return value;
+  }
+
+  template < class T >
+  T& Queue<T>::front() {
+    if (empty()) {
+      throw std::out_of_range("Queue is empty");
+    }
+    return data_.front();
+  }
+
+  template < class T >
+  const T& Queue<T>::front() const {
+    if (empty()) {
+      throw std::out_of_range("Queue is empty");
+    }
+    return data_.front();
+  }
+
+  template < class T >
+  bool Queue<T>::empty() const noexcept {
+    return data_.empty();
+  }
+
+  template < class T >
+  size_t Queue<T>::size() const noexcept {
+    return data_.size();
+  }
+
+  template < class T >
+  void Queue<T>::clear() {
+    data_.clear();
+  }
+}
 #endif
--- nepochatova.yulia/S2/stack.h
+++ nepochatova.yulia/S2/stack.h
@@ -2,12 +2,12 @@
 #define STACK_HPP
 
 #include "../common/BiList.hpp"
+#include <stdexcept>
+#include <utility>
 
-namespace nepochatova
-{
+namespace nepochatova {
   template < class T >
-  class Stack
-  {
+  class Stack {
   private:
     List< T > data_;
 
@@ -16,10 +16,55 @@ namespace nepochatova
     T drop();
     T & top();
     const T & top() const;
-    bool empty() const;
-    size_t size() const;
+    bool empty() const noexcept;
+    size_t size() const noexcept;
     void clear();
   };
+
+template < class T >
+ void Stack<T>::push(const T& value) {
+  data_.push_back(value);
 }
 
+template < class T >
+T Stack<T>::drop() {
+  if (empty()) {
+    throw std::out_of_range("Stack is empty");
+  }
+  T value = std::move(data_.back());
+  data_.pop_back();
+  return value;
+}
+
+template < class T >
+T& Stack<T>::top() {
+  if (empty()) {
+    throw std::out_of_range("Stack is empty");
+  }
+  return data_.back();
+}
+
+template < class T >
+const T& Stack<T>::top() const {
+  if (empty()) {
+    throw std::out_of_range("Stack is empty");
+  }
+  return data_.back();
+}
+
+template < class T >
+bool Stack<T>::empty() const noexcept {
+  return data_.empty();
+}
+
+template < class T >
+size_t Stack<T>::size() const noexcept {
+  return data_.size();
+}
+
+template < class T >
+void Stack<T>::clear() {
+  data_.clear();
+}
+}
 #endif
