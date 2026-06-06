c0a72aef4d8a810ee267ea6e290a52b1b29ad780
GenJune <julia.nepochatova@gmail.com>
2026-06-06 22:57:57 +0300

    added main

--- nepochatova.yulia/S4/main.cpp
+++ nepochatova.yulia/S4/main.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
+#include "commands.h"
 
-int main()
-{}
+int main(int argc, char* argv[])
+{
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " input_file\n";
+    return 1;
+  }
+  nepochatova::DictionaryManager manager;
+
+  try {
+    manager.loadFromFile(argv[1]);
+    manager.processCmds(std::cin, std::cout);
+  }
+  catch (const std::exception& e) {
+    std::cerr << e.what() << "\n";
+    return 1;
+  }
+
+  return 0;
+}
